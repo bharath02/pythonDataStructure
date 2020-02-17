f50216af1bafba54ac4dfee60f080595d36f01e5
bharath02 <fee0228@gmail.com>
2020-02-17 15:34:20 +0530

    Remove the first occurrence of a specified element of a array

---
+++ Array/4.0_removeFirstOccurrence.py
@@ -0,0 +1,11 @@
+# Write a Python program to remove the first occurrence of a specified element from an array. 
+class removeFirstOccurrence:
+    def mostRepeateRemove(self,List):
+        for i in range(len(List)-1):
+            for j in range(i+1,len(List)-1):
+                if(List[j]==List[i]):
+                    List.remove(List[j])
+lis=[1,2,5,6,9,1,1,9,5]
+obj=removeFirstOccurrence()
+obj.mostRepeateRemove(lis)
+print(lis)
