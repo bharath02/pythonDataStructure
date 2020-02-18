6b16a653ddd05107172944d4e0e68fb5e8a179b8
bharath02 <fee0228@gmail.com>
2020-02-18 15:43:35 +0530

    Remove List from the data UPDATE

--- Set/4.0_removeItemList.py
+++ Set/4.0_removeItemList.py
@@ -1,9 +1,11 @@
 # Write a Python program to remove item(s) from set
 class removeItemList:
     def listRemove(self,set1,set2):
-        setRemove=set1-set2
-        print(setRemove)
-set1=set(["Hi","!", "Hello","How","are","you","?","u"])
-set2=set(["Hello","u"])
+        set1.discard(10)
+        set2.discard('abs')
+        print(set1)
+        print(set2)
+set1=set([10,20,22,33,55,20])
+set2={'abs','Movie','Education','games','time','exams'}
 removeList=removeItemList()
 removeList.listRemove(set1,set2)
