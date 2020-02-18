1742e77bf375c8830b7524fe6de003352edda642
bharath02 <fee0228@gmail.com>
2020-02-18 14:59:53 +0530

    iteration of a set data type

---
+++ Set/2.0_iterationOverSets.py
@@ -0,0 +1,9 @@
+# Write a Python program to iteration over sets.
+class iterationSet:
+    def iterations(self,Set):
+       for iter in Set:
+            print(iter,end="")
+
+value=set("Bhcratd")
+ite=iterationSet()
+ite.iterations(value)
