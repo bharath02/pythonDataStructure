edc64d1a46b6cfa8404c7cc2fe3fce6732db2353
bharath02 <fee0228@gmail.com>
2020-02-19 10:08:31 +0530

    finding a smallest value with sorting Values

---
+++ List/3.0_smallestNumber.py
@@ -0,0 +1,16 @@
+#  Write a Python program to get the smallest number from a list.
+class smallestNumber:
+    def minimumNumber(self,list):
+        for num in range(len(list)-1):
+            for sort in range(len(list)-num-1):
+                if(list[sort]>list[sort+1]):
+                    list[sort],list[sort+1]=list[sort+1],list[sort]
+
+        print("smallest Number is : ", list[0])
+    def functionMin(self,list):
+        minVal=min(list)
+        print("Smallest value with function : ", minVal)
+list=[2,22,33,55,5,10]
+smallvalue=smallestNumber()
+smallvalue.minimumNumber(list)
+smallvalue.functionMin(list)
