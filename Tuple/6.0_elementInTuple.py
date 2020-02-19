60db766c6345142b8185c0bf54b61dd8968d9b3f
bharath02 <fee0228@gmail.com>
2020-02-19 16:45:06 +0530

    elements repeated in tuple

---
+++ Tuple/6.0_elementInTuple.py
@@ -0,0 +1,12 @@
+#  Write a Python program to check whether an element exists within a tuple.
+class existTuple:
+    def checkExistTup(self,Tup):
+        for val in Tup:
+            if key == val:
+                print(True)
+Tup=(1,2,3,4,5,6,7,8,9,10)
+key=10
+exist=existTuple()
+exist.checkExistTup(Tup)
+
+
