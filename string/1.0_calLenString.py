a8a92dd5fd6006d9ca645c4b0c4f9b7a0d47a21c
bharath02 <fee0228@gmail.com>
2020-02-19 17:51:39 +0530

    length of astring

---
+++ string/1.0_calLenString.py
@@ -0,0 +1,8 @@
+# write a Program to calculate the length of a string
+class lengthString:
+    def calString(self,string):
+        stringLength=len(string)
+        print(stringLength)
+string="hi this is for checking length of a string"
+lent=lengthString()
+lent.calString(string)
