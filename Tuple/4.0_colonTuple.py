7de98058bef101c56d723c0444a9108d519f52f2
bharath02 <fee0228@gmail.com>
2020-02-19 16:24:00 +0530

    add colon to tuple and append

---
+++ Tuple/4.0_colonTuple.py
@@ -0,0 +1,12 @@
+# write a Program to create a colon tuple
+from copy import deepcopy
+class colonTuple:
+    def tupleColon(self,Tup):
+        Tup_copy=deepcopy(Tup)
+        Tup_copy[2].append('e')
+        print(Tup_copy)
+        print(Tup)
+
+Tup=('a','b',[],'c','d')
+Tuple=colonTuple()
+Tuple.tupleColon(Tup)
