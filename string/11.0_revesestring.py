398f6668b1c83271db3eec9c915177387a2291db
bharath02 <fee0228@gmail.com>
2020-02-20 11:40:23 +0530

    n characteter lower case

--- string/11.0_revesestring.py
+++ string/11.0_revesestring.py
@@ -3,7 +3,7 @@ class reverseString:
     def strRev(self,String):
         for i in range(len(String)-1):
             val = String.split(" ")
-            
+
         print(" ".join(val[::-1]))
 sting="Mumbai is costly city then Bengaluru and Bengaluru is costly then Hyderabad"
 print(sting)
