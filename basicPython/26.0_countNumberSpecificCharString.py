be88de280f85d58670c66a4e438d051b3b3fbfed
bharath02 <fee0228@gmail.com>
2020-02-21 14:40:55 +0530

    count the number of specific characters

---
+++ basicPython/26.0_countNumberSpecificCharString.py
@@ -0,0 +1,7 @@
+# Write a Python program to count the number occurrence of a specific character in a string.
+class countSpecificCharacter:
+    def characterSpecial(self,string):
+        print("count of ! : ",string.count('!'))
+string='i like to learn! live hadoop in mumbai!, i is a word!'
+odj=countSpecificCharacter()
+odj.characterSpecial(string)
