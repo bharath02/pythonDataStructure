7ea4fd8b38e11d16184568382a268eb6deb970e2
bharath02 <fee0228@gmail.com>
2020-02-21 14:42:53 +0530

    clear screen or terminal with code

---
+++ basicPython/28.0_clearScreenTerminal.py
@@ -0,0 +1,10 @@
+# Write a Python program to clear the screen or terminal
+import os
+import time
+class clearScreenTerminal:
+    def clearScreen(self):
+        os.system("ls")
+        time.sleep(2)
+        os.system('clear')
+obj=clearScreenTerminal()
+obj.clearScreen()
